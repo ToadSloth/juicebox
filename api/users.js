6196e5e877e5f9cd17447dad1e9cee58afc46a5d
ToadSoth <cadewjarvis@yahoo.com>
2022-11-13 13:11:13 -0600

    commit

---
+++ api/users.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const usersRouter = express.Router();
+const { getAllUsers } = require('../db');
+
+usersRouter.use((req, res, next) => {
+    console.log("A request is being made to /users");
+  
+    next();
+  });
+  
+usersRouter.get('/', async (req, res) => {
+    console.log("anything")
+    try {
+         const users = await getAllUsers();
+    console.log('this is users', users)
+    res.send({
+      users
+    });
+    } catch(error) {
+        console.log(error)
+    }
+   
+  });
+
+module.exports = usersRouter;
